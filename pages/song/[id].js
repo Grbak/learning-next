f000f5da993617cd40cb86b0051867a15521142c
Vladislav Dybok <eden91hazard@gmail.com>
2022-10-02 01:33:29 +0300

    added loaders to the pages

--- pages/song/[id].js
+++ pages/song/[id].js
@@ -1,10 +1,37 @@
-import { useRouter } from "next/router"
+
+import { useState, useEffect } from 'react';
+import { useRouter, push } from "next/router"
 import { Layout } from "../../components/Layout";
 
-export default function Song({ data }) {
+export default function Song({ data: serverData }) {
+    const [data, setData] = useState(serverData);
     const router = useRouter();
+
+    useEffect(() => {
+        const load = async () => {
+            const response = await fetch (`http://localhost:4200/songs/${router.query.id}`);
+            const data = await response.json();
+            setData(data);
+        }
+
+        if (!data) {
+            load();
+        }
+    }, []);
+
+    if (!data) {
+        return (
+            <Layout>
+                <p>
+                    Loading...
+                </p>
+            </ Layout>
+        )
+    }
+
     return (
         <Layout>
+            <button onClick={() => push('/songs')}>Back to all songs</button>
             <h2>Song {router.query.id}</h2>
             <div>{JSON.stringify(data, null, 2)}</div>
         </Layout>
@@ -12,6 +39,11 @@ export default function Song({ data }) {
 }
 
 Song.getInitialProps = async function load(ctx) {
+    if(!ctx.req) {
+        return {
+            data: null
+        }
+    }
     const response = await fetch(`http://localhost:4200/songs/${ctx.query.id}`);
     const data = await response.json();
     return {
--- pages/songs.js
+++ pages/songs.js
@@ -1,13 +1,38 @@
+import { useState, useEffect } from 'react';
 import Router from 'next/router';
 import Link from 'next/link';
 import { Layout } from '../components/Layout';
 
-export default function Songs({ data }) {
+export default function Songs({ data: serverData }) {
+    const [data, setData] = useState(serverData);
+
+    useEffect(() => {
+        const load = async () => {
+            const response = await fetch('http://localhost:4200/songs');
+            const data = await response.json();
+            setData(data);
+        }
+
+        if (!serverData) {
+            load();
+        }
+    }, []);
+
+    if (!data) {
+        return (
+            <Layout>
+                <p>
+                    Loading...
+                </p>
+            </ Layout>
+        )
+    }
+
     return (
         <Layout title="Songs">
+            <button onClick={() => Router.push('/')}>Back to home page</button>
             <h2>Songs</h2>
             <p>You can see all your favourite songs on this page</p>
-            <button onClick={() => Router.push('/')}>Back to home page</button>
             <ul>
                 {data.map((song) => (
                     <li key={`${song.authot}-${song.title}`}>
@@ -23,7 +48,12 @@ export default function Songs({ data }) {
     );
 }
 
-Songs.getInitialProps = async function load() {
+Songs.getInitialProps = async function load(ctx) {
+    if (!ctx.req) {
+        return {
+            data: null
+        }
+    }
     const response = await fetch('http://localhost:4200/songs');
     const data = await response.json();
     
